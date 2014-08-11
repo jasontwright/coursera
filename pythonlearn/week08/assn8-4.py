fef6a6b5e493949e17e5846cab6d46b15af6bbc4
Jason Wright <jasontwright@gmail.com>
2014-08-10 21:41:01 -0500

    Completed week 8 exercises

--- pythonlearn/week08/assn8-4.py
+++ pythonlearn/week08/assn8-4.py
@@ -6,4 +6,9 @@ fname = raw_input("Enter file name: ")
 fh = open(fname)
 lst = list()
 for line in fh:
-print line.rstrip()
+  line = line.split()
+  for word in line:
+    if word in lst: continue
+    lst.append(word)
+    lst.sort()
+print lst
